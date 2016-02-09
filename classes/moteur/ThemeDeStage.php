6995082005b032271259333331e5123102e9d9e6
warnapipou <macary.francisque@gmail.com>
2016-02-09 16:38:45 +0100

    Probleme de getIdTheme qui retourne rien : ""

--- classes/moteur/ThemeDeStage.php
+++ classes/moteur/ThemeDeStage.php
@@ -1,48 +1,57 @@
 <?php
 
-	class ThemeDeStage {
-		var $idTheme;
-		var $theme;
+class ThemeDeStage {
+	var $idTheme;
+	var $theme;
 
-		/* Constructeur(s) */
-		public function ThemeDeStage($idtheme, $theme){
-			$this->idtheme = $idtheme;
-			$this->theme = $theme;
-		}
-
-		/* Accesseurs */
-		/*public function setIdTheme($idTheme){
-			$this->idTheme = $idTheme;
-		}
-
-		public function setTheme($theme){
-			$this->theme = $theme;
-		}
-		*/
-		public function getIdTheme(){
-			return $this->idTheme;
-		}
-
-		public function getTheme(){
-			return $this->theme;
-		}
-
-
-		/* Methode(s) */
-		public static function getListeTheme(){
-			$tabThemesStr = ThemeDeStage_BDD::getListeTheme();
-			
-			$tab_themes = array();
-			for($i=0; $i<sizeof($tabThemesStr); $i++)
-	  			array_push($tab_themes, new ThemeDeStage($tabThemesStr[$i][0], $tabThemesStr[$i][1]));
-	  			
-			return $tab_themes;
-		}
-
-		public static function saisirDonneesTheme($themeDeStage){
-			$theme = new ThemeDeStage('', $themeDeStage);
-			ThemeDeStage_BDD::sauvegarder($theme);
-		}
-
+	/* Constructeur(s) */
+	public function ThemeDeStage($idtheme, $theme){
+		$this->idtheme = $idtheme;
+		$this->theme = $theme;
 	}
+
+	/* Accesseurs */
+
+	public function setTheme($label){
+		$this->theme = $label;
+	}
+
+	public function getIdTheme(){
+		return $this->idTheme;
+	}
+
+	public function getTheme(){
+		return $this->theme;
+	}
+
+	public static function getThemeDeStage($id){
+		$themeDeStageStr = ThemeDeStage_BDD::getThemeDeStage($id);
+
+		$themeDeStage = new ThemeDeStage($themeDeStageStr['idtheme'], $themeDeStageStr['theme']);
+
+		return $themeDeStage;
+	}
+
+
+	/* Methode(s) */
+	public static function getListeTheme(){
+		$tabThemesStr = ThemeDeStage_BDD::getListeTheme();
+
+		$tab_themes = array();
+		for($i=0; $i<sizeof($tabThemesStr); $i++)
+  			array_push($tab_themes, new ThemeDeStage($tabThemesStr[$i][0], $tabThemesStr[$i][1]));
+  			
+		return $tab_themes;
+	}
+
+	public static function saisirDonneesTheme($themeDeStage){
+		$theme = new ThemeDeStage('', $themeDeStage);
+		ThemeDeStage_BDD::sauvegarder($theme);
+	}
+
+	public static function deleteTheme($themeDeStage){
+		ThemeDeStage_BDD::delete($themeDeStage);
+	}
+
+}
 ?>
---
+++ gestion/conventions/mod_themeDeStage.php
@@ -0,0 +1,27 @@
+<?php
+$chemin = "../../classes/";
+include_once($chemin."bdd/connec.inc");
+include_once($chemin."bdd/ThemeDeStage_BDD.php");
+include_once($chemin."ihm/IHM_Generale.php");
+include_once($chemin."moteur/ThemeDeStage.php");
+$tabLiens = array();
+$tabLiens[0] = array('../../', 'Accueil');
+$tabLiens[1] = array('../', 'Gestion de la base');
+
+IHM_Generale::header("Modifier un ", "thème de stage", "../../", $tabLiens);
+
+function modifier(){
+	if($_POST['id']!=-1){
+		$theme=ThemeDeStage::getThemeDeStage($_POST['id']);
+		$theme->setTheme($_POST['label']);
+		ThemeDeStage_BDD::sauvegarder($theme);
+		printf("Le thème de stage a été modifié ! ");
+	}else {
+		IHM_Generale::erreur("Vous devez sélectionner un thème de stage !");
+	}
+}
+modifier();
+printf("<p><a href='../../gestion/conventions/modifierThemeDeStage.php'>Retour</a></p>");
+IHM_Generale::endHeader(false);
+IHM_Generale::footer("../../");
+?>
