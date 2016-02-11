86fd756f433dc772f3b025cba32241842d08f68c
lorindr <vincent.avenein@openmailbox.org>
2016-02-11 14:39:30 +0100

    ajax fonctionnel mais bug sur le rechargement des pie

--- gestion/statistiques/statistiques.php
+++ gestion/statistiques/statistiques.php
@@ -1,278 +1,39 @@
 <?php
 
 $chemin = '../../classes/';
+include_once($chemin."bdd/connec.inc");
+include_once($chemin."bdd/Etudiant_BDD.php");
+include_once($chemin."bdd/Filiere_BDD.php");
+include_once($chemin."bdd/Parcours_BDD.php");
+include_once($chemin."bdd/Promotion_BDD.php");
+include_once($chemin."ihm/IHM_Generale.php");
+include_once($chemin."ihm/Promotion_IHM.php");
+include_once($chemin."moteur/Etudiant.php");
+include_once($chemin."moteur/Filiere.php");
 include_once($chemin."moteur/Filtre.php");
+include_once($chemin."moteur/FiltreNumeric.php");
+include_once($chemin."moteur/FiltreString.php");
+include_once($chemin."moteur/Parcours.php");
+include_once($chemin."moteur/Promotion.php");
+include_once("frameworksJS/statistique_fonctions.php");
+
+
+
+$tabLiens = array();
+$tabLiens[0] = array('../../', 'Accueil');
+$tabLiens[1] = array('../', 'Gestion de la base');
+IHM_Generale::header("Statistiques", "entreprises", "../../", $tabLiens);
+
+Statistiques::selectionAnnee(false);
+
+
+echo "<br/>";
+echo "<br/>";
+echo "<div id='data'>\n";
 include_once("statistiquesData.php");
+echo "\n</div>";
 
 
-
+IHM_Generale::endHeader(false);
+IHM_Generale::footer("../../");
 ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Statistiques Reloaded</title>
-		<script src="frameworksJS/jquery.js"></script>
-		<script src="frameworksJS/Chart.js"></script>
-		<style type="text/css">
-
-			
-		</style>
-	</head>
-	<body>
-		<h1>Ann&eacute;e <?php echo $annee;?></h1>
-		
-		<section id="section_gauche">
-			<h2 style="color:LightSkyBlue">Promotion M1</h2>
-			
-				<table  >
-					<th colspan=3>Lieu du stage</th>
-					<tr>
-						<td bgcolor=red ></td>
-						<td >Le Mans</td>
-						<td><?php echo $mansM1;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=orange ></td>
-						<td>Sarthe</td>
-						<td><?php echo $sartheM1;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=green ></td>
-						<td>Pays de la Loire</td>
-						<td><?php echo $regionM1;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=blue ></td>
-						<td>France</td>
-						<td><?php echo $franceM1;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=darkviolet></td>
-						<td>Monde</td>
-						<td><?php echo $mondeM1;?></td>
-					</tr>
-				</table>
-
-				</br></br>
-				<canvas id="mycanvas" width="256" height="256">
-				</canvas>
-
-				<script>
-					$(document).ready(function(){
-						var ctx = $("#mycanvas").get(0).getContext("2d");
-						//pie chart data
-						//sum of values = peu importe
-						
-						var data = [
-							{
-								value: <?php echo $mansM1;?>,
-								color: "red",
-								highlight: "darkred",
-								label:  "Le Mans "					},
-							{
-								value: <?php echo $sartheM1;?>,
-								color: "orange",
-								highlight: "darkorange",
-								label: "Sarthe "
-							},
-							{
-								value: <?php echo $regionM1;?>,
-								color: "green",
-								highlight: "darkgreen",
-								label: "Pays de la Loire "
-							},
-							{
-								value: <?php echo $franceM1;?>,
-								color: "blue",
-								highlight: "darkblue",
-								label: "France "
-							},
-							{
-								value: <?php echo $mondeM1;?>,
-								color: "darkviolet",
-								highlight: "indigo",
-								label: "Monde "
-							}
-						];
-						//draw
-						var piechart = new Chart(ctx).Pie(data, { animateScale: true});
-						//var linechart = new Chart(ctx).Line(data);
-					});
-				</script>
-			
-		</section>
-
-
-
-		
-		<section id="section_centre">
-			<h2 style="color:SteelBlue">Promotion M2</h2>
-			
-				<table  >
-					<th colspan=3>Lieu du stage</th>
-					<tr>
-						<td bgcolor=red ></td>
-						<td >Le Mans</td>
-						<td><?php echo $mansM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=orange ></td>
-						<td>Sarthe</td>
-						<td><?php echo $sartheM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=green ></td>
-						<td>Pays de la Loire</td>
-						<td><?php echo $regionM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=blue ></td>
-						<td>France</td>
-						<td><?php echo $franceM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=darkviolet></td>
-						<td>Monde</td>
-						<td><?php echo $mondeM2;?></td>
-					</tr>
-				</table>
-
-				</br></br>
-			
-				<canvas id="mycanvas2" width="256" height="256">
-				</canvas>
-
-				<script>
-					$(document).ready(function(){
-						var ctx = $("#mycanvas2").get(0).getContext("2d");
-						//pie chart data
-						//sum of values = peu importe
-						
-						var data = [
-							{
-								value: <?php echo $mansM2;?>,
-								color: "red",
-								highlight: "darkred",
-								label:  "Le Mans "					},
-							{
-								value: <?php echo $sartheM2;?>,
-								color: "orange",
-								highlight: "darkorange",
-								label: "Sarthe "
-							},
-							{
-								value: <?php echo $regionM2;?>,
-								color: "green",
-								highlight: "darkgreen",
-								label: "Pays de la Loire "
-							},
-							{
-								value: <?php echo $franceM2;?>,
-								color: "blue",
-								highlight: "darkblue",
-								label: "France "
-							},
-							{
-								value: <?php echo $mondeM2;?>,
-								color: "darkviolet",
-								highlight: "indigo",
-								label: "Monde "
-							}
-						];
-						//draw
-						var piechart = new Chart(ctx).Pie(data, { animateScale: true});
-						//var linechart = new Chart(ctx).Line(data);
-					});
-				</script>
-			
-		</section>
-
-
-		
-		<section id="section_droite">
-			<h2 style="color:RoyalBlue">Promotion Master</h2>
-			
-				<table  >
-					<th colspan=3>Lieu du stage</th>
-					<tr>
-						<td bgcolor=red ></td>
-						<td >Le Mans</td>
-						<td><?php echo $mansM1+$mansM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=orange ></td>
-						<td>Sarthe</td>
-						<td><?php echo $sartheM1+$sartheM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=green ></td>
-						<td>Pays de la Loire</td>
-						<td><?php echo $regionM1+$regionM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=blue ></td>
-						<td>France</td>
-						<td><?php echo $franceM1+$franceM2;?></td>
-					</tr>
-					<tr>
-						<td bgcolor=darkviolet></td>
-						<td>Monde</td>
-						<td><?php echo $mondeM1+$mondeM2;?></td>
-					</tr>
-				</table>
-
-				</br></br>
-			
-
-			
-				<canvas id="mycanvas3" width="256" height="256">
-				</canvas>
-
-				<script>
-					$(document).ready(function(){
-						var ctx = $("#mycanvas3").get(0).getContext("2d");
-						//pie chart data
-						//sum of values = peu importe
-						
-						var data = [
-							{
-								value: <?php echo $mansM1+$mansM2;?>,
-								color: "red",
-								highlight: "darkred",
-								label:  "Le Mans "					},
-							{
-								value: <?php echo $sartheM1+$sartheM2;?>,
-								color: "orange",
-								highlight: "darkorange",
-								label: "Sarthe "
-							},
-							{
-								value: <?php echo $regionM1+$regionM2;?>,
-								color: "green",
-								highlight: "darkgreen",
-								label: "Pays de la Loire "
-							},
-							{
-								value: <?php echo $franceM1+$franceM2;?>,
-								color: "blue",
-								highlight: "darkblue",
-								label: "France "
-							},
-							{
-								value: <?php echo $mondeM1+$mondeM2;?>,
-								color: "darkviolet",
-								highlight: "indigo",
-								label: "Monde "
-							}
-						];
-						//draw
-						var piechart = new Chart(ctx).Pie(data, { animateScale: true});
-						//var linechart = new Chart(ctx).Line(data);
-					});
-				</script>
-			
-		</section>
-				
-	</body>
-</html>
--- gestion/statistiques/statistiquesData.php
+++ gestion/statistiques/statistiquesData.php
@@ -33,17 +33,14 @@ include_once($chemin."moteur/Filtre.php");
 include_once($chemin."moteur/FiltreNumeric.php");
 include_once($chemin."moteur/FiltreString.php");
 
-$tabLiens = array();
-$tabLiens[0] = array('../../', 'Accueil');
-$tabLiens[1] = array('../', 'Gestion de la base');
-IHM_Generale::header("Statistiques", "entreprises", "../../", $tabLiens);
-
-Promotion_IHM::afficherFormulaireSelectionAnnee(true);
-
 //recuperer une année universitaire
 
 //construire un filtre selon la bonne année universitaire
-$annee = "2014";
+if (!isset($_POST['annee'])) {
+	$annee = "2015";
+} else {
+	$annee = $_POST['annee'];
+}
 $parcoursISI = "9";
 $filiereMaster1 = "4";
 $filiereMaster2 = "1";
@@ -153,4 +150,260 @@ if(sizeof($conventionM2)>0) {
 	}
 }
 
+
 ?>
+
+<h1>Ann&eacute;e <?php echo $annee;?></h1>
+
+<section id="section_gauche">
+	<h2 style="color:LightSkyBlue">Promotion M1</h2>
+	
+		<table  >
+			<th colspan=3>Lieu du stage</th>
+			<tr>
+				<td bgcolor=red ></td>
+				<td >Le Mans</td>
+				<td><?php echo $mansM1;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=orange ></td>
+				<td>Sarthe</td>
+				<td><?php echo $sartheM1;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=green ></td>
+				<td>Pays de la Loire</td>
+				<td><?php echo $regionM1;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=blue ></td>
+				<td>France</td>
+				<td><?php echo $franceM1;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=darkviolet></td>
+				<td>Monde</td>
+				<td><?php echo $mondeM1;?></td>
+			</tr>
+		</table>
+
+		</br></br>
+		<canvas id="mycanvas" width="256" height="256">
+		</canvas>
+
+		<script>
+			$(document).ready(function(){
+				var ctx = $("#mycanvas").get(0).getContext("2d");
+				//pie chart data
+				//sum of values = peu importe
+				
+				var data = [
+					{
+						value: <?php echo $mansM1;?>,
+						color: "red",
+						highlight: "darkred",
+						label:  "Le Mans "					},
+					{
+						value: <?php echo $sartheM1;?>,
+						color: "orange",
+						highlight: "darkorange",
+						label: "Sarthe "
+					},
+					{
+						value: <?php echo $regionM1;?>,
+						color: "green",
+						highlight: "darkgreen",
+						label: "Pays de la Loire "
+					},
+					{
+						value: <?php echo $franceM1;?>,
+						color: "blue",
+						highlight: "darkblue",
+						label: "France "
+					},
+					{
+						value: <?php echo $mondeM1;?>,
+						color: "darkviolet",
+						highlight: "indigo",
+						label: "Monde "
+					}
+				];
+				//draw
+				var piechart = new Chart(ctx).Pie(data, { animateScale: true});
+				//var linechart = new Chart(ctx).Line(data);
+			});
+		</script>
+	
+</section>
+
+
+
+
+<section id="section_centre">
+	<h2 style="color:SteelBlue">Promotion M2</h2>
+	
+		<table  >
+			<th colspan=3>Lieu du stage</th>
+			<tr>
+				<td bgcolor=red ></td>
+				<td >Le Mans</td>
+				<td><?php echo $mansM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=orange ></td>
+				<td>Sarthe</td>
+				<td><?php echo $sartheM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=green ></td>
+				<td>Pays de la Loire</td>
+				<td><?php echo $regionM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=blue ></td>
+				<td>France</td>
+				<td><?php echo $franceM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=darkviolet></td>
+				<td>Monde</td>
+				<td><?php echo $mondeM2;?></td>
+			</tr>
+		</table>
+
+		</br></br>
+	
+		<canvas id="mycanvas2" width="256" height="256">
+		</canvas>
+
+		<script>
+			$(document).ready(function(){
+				var ctx = $("#mycanvas2").get(0).getContext("2d");
+				//pie chart data
+				//sum of values = peu importe
+				
+				var data = [
+					{
+						value: <?php echo $mansM2;?>,
+						color: "red",
+						highlight: "darkred",
+						label:  "Le Mans "					},
+					{
+						value: <?php echo $sartheM2;?>,
+						color: "orange",
+						highlight: "darkorange",
+						label: "Sarthe "
+					},
+					{
+						value: <?php echo $regionM2;?>,
+						color: "green",
+						highlight: "darkgreen",
+						label: "Pays de la Loire "
+					},
+					{
+						value: <?php echo $franceM2;?>,
+						color: "blue",
+						highlight: "darkblue",
+						label: "France "
+					},
+					{
+						value: <?php echo $mondeM2;?>,
+						color: "darkviolet",
+						highlight: "indigo",
+						label: "Monde "
+					}
+				];
+				//draw
+				var piechart = new Chart(ctx).Pie(data, { animateScale: true});
+				//var linechart = new Chart(ctx).Line(data);
+			});
+		</script>
+	
+</section>
+
+
+
+<section id="section_droite">
+	<h2 style="color:RoyalBlue">Promotion Master</h2>
+	
+		<table  >
+			<th colspan=3>Lieu du stage</th>
+			<tr>
+				<td bgcolor=red ></td>
+				<td >Le Mans</td>
+				<td><?php echo $mansM1+$mansM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=orange ></td>
+				<td>Sarthe</td>
+				<td><?php echo $sartheM1+$sartheM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=green ></td>
+				<td>Pays de la Loire</td>
+				<td><?php echo $regionM1+$regionM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=blue ></td>
+				<td>France</td>
+				<td><?php echo $franceM1+$franceM2;?></td>
+			</tr>
+			<tr>
+				<td bgcolor=darkviolet></td>
+				<td>Monde</td>
+				<td><?php echo $mondeM1+$mondeM2;?></td>
+			</tr>
+		</table>
+
+		</br></br>
+	
+
+	
+		<canvas id="mycanvas3" width="256" height="256">
+		</canvas>
+
+		<script>
+			$(document).ready(function(){
+				var ctx = $("#mycanvas3").get(0).getContext("2d");
+				//pie chart data
+				//sum of values = peu importe
+				
+				var data = [
+					{
+						value: <?php echo $mansM1+$mansM2;?>,
+						color: "red",
+						highlight: "darkred",
+						label:  "Le Mans "					},
+					{
+						value: <?php echo $sartheM1+$sartheM2;?>,
+						color: "orange",
+						highlight: "darkorange",
+						label: "Sarthe "
+					},
+					{
+						value: <?php echo $regionM1+$regionM2;?>,
+						color: "green",
+						highlight: "darkgreen",
+						label: "Pays de la Loire "
+					},
+					{
+						value: <?php echo $franceM1+$franceM2;?>,
+						color: "blue",
+						highlight: "darkblue",
+						label: "France "
+					},
+					{
+						value: <?php echo $mondeM1+$mondeM2;?>,
+						color: "darkviolet",
+						highlight: "indigo",
+						label: "Monde "
+					}
+				];
+				//draw
+				var piechart = new Chart(ctx).Pie(data, { animateScale: true});
+				//var linechart = new Chart(ctx).Line(data);
+			});
+		</script>
+	
+</section>
+
