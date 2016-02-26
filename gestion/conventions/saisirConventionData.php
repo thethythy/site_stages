e2e65ceede3820152a19f61951fbbf301814e27d
warnapipou <macary.francisque@gmail.com>
2016-02-26 17:02:16 +0100

    Edition des compétences et corrections

--- gestion/conventions/saisirConventionData.php
+++ gestion/conventions/saisirConventionData.php
@@ -34,7 +34,7 @@ include_once($chemin."moteur/ThemeDeStage.php");
 
 // Précisons l'encodage des données si cela n'est pas déjà fait
 if (!headers_sent())
-	header("Content-type: text/html; charset=iso-8859-15");
+	header("Content-type: text/html; charset=utf-8");
 
 // Prise en compte des paramètres
 $filtres = array();
---
+++ gestion/entreprises/modifierCompetence.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once("../../classes/bdd/connec.inc");
+include_once("../../classes/bdd/Competence_BDD.php");
+include_once("../../classes/ihm/IHM_Generale.php");
+include_once("../../classes/ihm/Competence_IHM.php");
+include_once("../../classes/moteur/Competence.php");
+
+$tabLiens = array();
+$tabLiens[0] = array('../../', 'Accueil');
+$tabLiens[1] = array('../', 'Gestion de la base');
+IHM_Generale::header("Modifier une", "compétence", "../../",$tabLiens);
+
+
+function modifier() {
+
+
+	if(isset($_POST['label'])){
+		$competence = new Competence($_POST['id'], $_POST['label']);
+		Competence_BDD::sauvegarder($competence);
+		printf("La compétence a été modifié ! ");	}
+	else{
+		$competence = Competence::getCompetence($_GET['id']);
+
+	    printf("<h2>Modification d'une compétence</h2>");
+	    printf("<center><form action='modifierCompetence.php' method=post>\n");
+	    printf("<td><input type=hidden name='id' size=100 value=%s></td>\n", $competence->getIdentifiantBDD());
+	    printf("<table><center><tr><td>Compétence : </td>\n");
+	    printf("<td><input name='label' size=100 value=%s></td>\n", $competence->getNom());
+	    printf("<table><tr><td><input type=submit value='Modifier'/></center></td></tr>");
+	    printf("</table></form></center>");
+	}
+}
+
+
+
+modifier();
+deconnexion();
+printf("<p><a href='../../gestion/entreprises/gestionCompetence.php'>Retour</a></p>");
+IHM_Generale::endHeader(false);
+IHM_Generale::footer("../../");
+
+?>
