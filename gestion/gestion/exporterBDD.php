6094e1a707aad59a70f76178ac7bce38d3dae26a
warnapipou <macary.francisque@gmail.com>
2016-03-02 17:54:17 +0100

    Corrections + debut exportation

--- gestion/gestion/exporterBDD.php
+++ gestion/gestion/exporterBDD.php
@@ -1,32 +1,35 @@
 <?php 
 $chemin = "../../classes/";
-
+include_once($chemin."ihm/IHM_Generale.php");
 include_once($chemin . "bdd/connec.inc");
+$tabLiens = array();
+$tabLiens[0] = array('../../', 'Accueil');
+$tabLiens[1] = array('../', 'Exportation de la base');
+
+IHM_Generale::header("Exporter la ", "base de données", "../../", $tabLiens);
+
 
 function export(){
 	global $db;
+	global $tab23;
 	
-	$date = date("d-m-Y");
+	$date = date("Y-m-d");
+	$backup = "../../telechargements/base_stage_".$date.".sql.zip";
+	$tabulation = "\t";
+	$retour = "\n";
 
+	$sql = "SELECT * INTO OUTFILE $backup 
+				FIELDS TERMINATED BY ".$tabulation." LINES TERMINATED BY ".$retour." 
+				FROM $tab23";
 
+	if($db->query($sql))
+		echo "Success !";
+	echo "$sql";
 }
 
-$backup = $db."bdd-backup_".$date.".sql.gz";
-// Utilise les fonctions système : MySQLdump & GZIP pour générer un backup gzipé
-$command = "mysqldump --databases $db -h$host -u$user -p$pass | gzip> $backup";
-system($command);
-// Démarre la procédure de téléchargement
-$taille = filesize($backup);
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Cache-Control: public");
-header("Content-Description: File Transfer");
-header("Content-Type: application/gzip");
-header("Content-Disposition: attachment; filename=$backup;");
-header("Content-Transfer-Encoding: binary");
-header("Content-Length: ".$taille);
-@readfile($backup);
-// Supprime le fichier temporaire du serveur
-unlink($backup);
+export();
+deconnexion();
+
+IHM_Generale::endHeader(false);
+IHM_Generale::footer("../../");
 ?>
