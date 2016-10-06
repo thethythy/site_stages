025bb79d9fc9a65ce574330c3f4bbcfb9860bc45
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2016-10-06 16:19:04 +0200

    Ajout du lien vers "Career Center" dans la partie stagiaire

--- classes/ihm/Menu.php
+++ classes/ihm/Menu.php
@@ -64,6 +64,7 @@ class Menu {
 		    <br></br>
 		    <li>Site externes d'offres:</li>
 		    <ul class="sidemenu2">
+			<li><a href="http://univ-lemans.jobteaser.com/" title="Career Center">Career Center</a></li>
 			<li><a href="http://www.ca-recrute-en.sarthe.com/index.asp" title="ca-recrute-en-sarthe.com">ça recrute en Sarthe</a></li>
 			<li><a href="http://offres.monster.fr" title="monster.fr">monster.fr</a></li>
 			<li><a href="http://www.lerucher.com/" title="Le Rucher">Le Rucher</a></li>
