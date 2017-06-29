eeb538226d84e592f40ca67269a475ea9b269ba1
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2017-06-29 15:56:02 +0200

    Modification de la fonction removeaccents pour fonctionner en UTF-8

--- classes/moteur/Utils.php
+++ classes/moteur/Utils.php
@@ -13,11 +13,25 @@ class Utils {
 
     // Fonction qui enlève les caractères accentués
     public static function removeaccents($string) {
-	return strtr(
-		strtr($string, 'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõöøùúûüýÿ',
-			       'AAAAAACEEEEIIIINOOOOOOUUUUYaaaaaaceeeeiiiinoooooouuuuyy'),
-		array('Þ' => 'TH', 'þ' => 'th', 'Ð' => 'DH', 'ð' => 'dh', 'ß' => 'ss',
-	    '' => 'OE', '' => 'oe', 'Æ' => 'AE', 'æ' => 'ae', 'µ' => 'u', ' ' => '_'));
+	return strtr($string, array('À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A' ,'Å' => 'A',
+				    'Ç' => 'C',
+				    'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+				    'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+				    'Ñ' => 'N',
+				    'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O',
+				    'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U',
+				    'Ý' => 'Y',
+				    'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a',
+				    'ç' =>  'c',
+				    'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e',
+				    'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+				    'ñ' => 'n',
+				    'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o',
+				    'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u',
+				    'ý' => 'y', 'ÿ' => 'y',
+				    'Æ' => 'AE', 'æ' => 'ae',
+				    'µ' => 'u',
+				    ' ' => '_'));
     }
 
     // Fonction de recherche d'un étudiant dans le ldap de l'ic2
