d922efbde688cc8d09a2438eabe2af25979c5ba3
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2017-09-20 16:38:58 +0200

    Mise à jour des fichiers téléchargeables pour la nouvelle année universitaire

--- telechargements/index.php
+++ telechargements/index.php
@@ -17,9 +17,9 @@ IHM_Generale::header("", "Téléchargements", "../", $tabLiens);
 <h4>Présentations orales</h4>
 
 <ul>
-    <li>Présentation détaillée 2016-2017 (format PDF) <A href="./presentation-1617.pdf"><IMG border=0 title="Présentation détaillée" align=middle src="../images/download.png"/></A></li>
-    <li>Présentation aux étudiants de M2 2016-2017 (format PDF) <A href="./presentation-M2-1617.pdf"><IMG border=0 title="Présentation M2" align=middle src="../images/download.png"/></A></li>
-    <li>Présentation aux étudiants de M1 2016-2017 (format PDF) <A href="./presentation-M1-1617.pdf"><IMG border=0 title="Présentation M1" align=middle src="../images/download.png"/></A></li>
+    <li>Présentation détaillée 2017-2018 (format PDF) <A href="./presentation-1718.pdf"><IMG border=0 title="Présentation détaillée" align=middle src="../images/download.png"/></A></li>
+    <li>Présentation aux étudiants de M2 2017-2018 (format PDF) <A href="./presentation-M2-1718.pdf"><IMG border=0 title="Présentation M2" align=middle src="../images/download.png"/></A></li>
+    <li>Présentation aux étudiants de M1 2017-2018 (format PDF) <A href="./presentation-M1-1718.pdf"><IMG border=0 title="Présentation M1" align=middle src="../images/download.png"/></A></li>
 </ul>
 
 <h4>Documents annexes</h4>
@@ -27,15 +27,15 @@ IHM_Generale::header("", "Téléchargements", "../", $tabLiens);
 <ul>
 <li>Annexe 1 - Exemple d'extrait de rapport de stage (format PDF) <a href="./Annexe1.pdf"><IMG border=0 title="Fiche extrait" align=middle src="../images/download.png"/></a></li>
 <li>Annexe 2 - Norme de présentation du rapport de stage (format PDF) <a href="./Annexe2.pdf"><IMG border=0 title="Fiche rapport" align=middle src="../images/download.png"/></a></li>
-<li>Annexe 3 - Fiche d'évaluation 2016-2017 pour l'entreprise
-    (format DOC) <A href="./Fiche_Entreprise_1617.doc"><IMG border=0 title="Fiche entreprise format DOC" align=middle src="../images/download.png"/></A>
-    (format PDF) <A href="./Fiche_Entreprise_1617.pdf"><IMG border=0 title="Fiche entreprise format PDF" align=middle src="../images/download.png"/></A></li>
-<li>Annexe 3bis - Fiche d'évaluation 2016-2017 pour l'entreprise version anglaise
-    (format DOC) <A href="./Entreprise_Form_1617.doc"><IMG border=0 title="Fiche entreprise version anglaise format DOC" align=middle src="../images/download.png"/></A>
-    (format PDF) <A href="./Entreprise_Form_1617.pdf"><IMG border=0 title="Fiche entreprise version anglaise format PDF" align=middle src="../images/download.png"/></A></li>
-<li>Annexe 4 - Fiche de soutenance 2016-2017 pour le jury
-    (format DOC) <A href="./Fiche_Soutenance_1617.doc"><IMG border=0 title="Fiche soutenance format DOC" align=middle src="../images/download.png"/></A>
-    (format PDF) <A href="./Fiche_Soutenance_1617.pdf"><IMG border=0 title="Fiche soutenance format PDF" align=middle src="../images/download.png"/></A></li>
+<li>Annexe 3 - Fiche d'évaluation 2017-2018 pour l'entreprise
+    (format DOC) <A href="./Fiche_Entreprise_1718.doc"><IMG border=0 title="Fiche entreprise format DOC" align=middle src="../images/download.png"/></A>
+    (format PDF) <A href="./Fiche_Entreprise_1718.pdf"><IMG border=0 title="Fiche entreprise format PDF" align=middle src="../images/download.png"/></A></li>
+<li>Annexe 3bis - Fiche d'évaluation 2017-2018 pour l'entreprise version anglaise
+    (format DOC) <A href="./Entreprise_Form_1718.doc"><IMG border=0 title="Fiche entreprise version anglaise format DOC" align=middle src="../images/download.png"/></A>
+    (format PDF) <A href="./Entreprise_Form_1718.pdf"><IMG border=0 title="Fiche entreprise version anglaise format PDF" align=middle src="../images/download.png"/></A></li>
+<li>Annexe 4 - Fiche de soutenance 2017-2018 pour le jury
+    (format DOC) <A href="./Fiche_Soutenance_1718.doc"><IMG border=0 title="Fiche soutenance format DOC" align=middle src="../images/download.png"/></A>
+    (format PDF) <A href="./Fiche_Soutenance_1718.pdf"><IMG border=0 title="Fiche soutenance format PDF" align=middle src="../images/download.png"/></A></li>
 </ul>
 
 <h4>Documents officiels</h4>
