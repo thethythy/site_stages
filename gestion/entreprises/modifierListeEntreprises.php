7f190a7fa19b4908f3862841a952ed6c45590c93
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2018-06-27 11:58:33 +0200

    Ajout d'un traitement Ajax à la page de modification/supression des entreprises

--- gestion/entreprises/modifierListeEntreprises.php
+++ gestion/entreprises/modifierListeEntreprises.php
@@ -2,7 +2,8 @@
 
 /**
  * Page modifierListeEntreprises.php
- * Utilisation : page pour modifier ou supprimer une entreprise parmi une liste
+ * Utilisation : page pour modifier ou supprimer une entreprise parmi une liste filtrée
+ * Dépendance(s) : modifierListeEntreprisesData.php --> traitement des requêtes Ajax
  * Accès : restreint par authentification HTTP
  */
 
@@ -17,6 +18,9 @@ $tabLiens[1] = array('../', 'Gestion de la base');
 
 IHM_Generale::header("Modifier/Supprimer une", "entreprise", "../../", $tabLiens);
 
+// ----------------------------------------------------------------------------
+// Contrôleur
+
 if (isset($_GET['id'])) {
     // Nécessaire pour que dans le formulaire de recherche, on resélectionne les valeurs précédement sélectionnées
     $_POST['nom'] = $_GET['nom'];
@@ -28,57 +32,17 @@ if (isset($_GET['id'])) {
     Entreprise::supprimerEntreprise($_GET['id']);
 }
 
-Entreprise_IHM::afficherFormulaireRecherche("modifierListeEntreprises.php");
+// ----------------------------------------------------------------------------
+// Affichage
 
-// Si une recherche a été effectuée
-if ((isset($_POST['rech'])) || (isset($_GET['id']))) {
-    $nom = $_POST['nom'];
-    $cp = $_POST['cp'];
-    $ville = $_POST['ville'];
-    $pays = $_POST['pays'];
-} else {
-    $nom = $cp = $ville = $pays = "";
-}
+Entreprise_IHM::afficherFormulaireRecherche("modifierListeEntreprisesData.php");
 
-$filtres = array();
+// Affichage des données
+echo "<div id='data'>\n";
+include_once("modifierListeEntreprisesData.php");
+echo "\n</div>";
 
-// Si une recherche sur le nom de l'entreprise est demandée
-if ($nom != "")
-    array_push($filtres, new FiltreString("nom", "%" . $nom . "%"));
-
-// Si une recherche sur le code postal est demandée
-if ($cp != "")
-    array_push($filtres, new FiltreString("codepostal", $cp . "%"));
-
-// Si une recherche sur la ville est demandée
-if ($ville != "")
-    array_push($filtres, new FiltreString("ville", $ville . "%"));
-
-// Si une recherche sur le pays est demandée
-if ($pays != "")
-    array_push($filtres, new FiltreString("pays", $pays . "%"));
-
-$nbFiltres = sizeof($filtres);
-
-if ($nbFiltres >= 2) {
-    $filtre = $filtres[0];
-    for ($i = 1; $i < sizeof($filtres); $i++)
-	$filtre = new Filtre($filtre, $filtres[$i], "AND");
-} else if ($nbFiltres == 1) {
-    $filtre = $filtres[0];
-} else {
-    $filtre = "";
-}
-
-$tabEntreprises = Entreprise::getListeEntreprises($filtre);
-
-// Si il y a au moins une entreprise
-if (sizeof($tabEntreprises) > 0) {
-    // Affichage des entreprises correspondants aux critères de recherches
-    Entreprise_IHM::afficherListeEntrepriseAEditer($tabEntreprises);
-}else {
-    echo "Aucune entreprise ne correspond aux critères de recherche.";
-}
+deconnexion();
 
 IHM_Generale::endHeader(false);
 IHM_Generale::footer("../../");
---
+++ gestion/entreprises/modifierListeEntreprisesData.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Page modifierListeEntrepriseData.php
+ * Utilisation : page de traitement Ajax retournant un tableau interactif des
+ *		 entreprises filtrées et classées par orde alphabétique
+ * Accès : restreint par authentification HTTP
+ */
+
+include_once("../../classes/bdd/connec.inc");
+
+include_once('../../classes/moteur/Utils.php');
+spl_autoload_register('Utils::my_autoloader_from_level2');
+
+// Précisons l'encodage des données si cela n'est pas déjà fait
+if (!headers_sent())
+    header('Content-type: text/html; charset=utf-8');
+
+// -----------------------------------------------------------------------------
+// Création des filtres
+
+$filtres = array();
+
+// Si une recherche sur le nom de l'entreprise est demandée
+if (isset($_POST['nom']) && $_POST['nom'] != "")
+    array_push($filtres, new FiltreString("nom", "%" . $_POST['nom'] . "%"));
+
+// Si une recherche sur le code postal est demandée
+if (isset($_POST['cp']) && $_POST['cp'] != "")
+    array_push($filtres, new FiltreString("codepostal", $_POST['cp'] . "%"));
+
+// Si une recherche sur la ville est demandée
+if (isset($_POST['ville']) && $_POST['ville'] != "")
+    array_push($filtres, new FiltreString("ville", $_POST['ville'] . "%"));
+
+// Si une recherche sur le pays est demandée
+if (isset($_POST['pays']) && $_POST['pays'] != "")
+    array_push($filtres, new FiltreString("pays", $_POST['pays'] . "%"));
+
+$nbFiltres = sizeof($filtres);
+
+if ($nbFiltres >= 2) {
+    $filtre = $filtres[0];
+    for ($i = 1; $i < sizeof($filtres); $i++)
+	$filtre = new Filtre($filtre, $filtres[$i], "AND");
+} else if ($nbFiltres == 1) {
+    $filtre = $filtres[0];
+} else {
+    $filtre = "";
+}
+
+// -----------------------------------------------------------------------------
+// Affichage des données
+
+$tabEntreprises = Entreprise::getListeEntreprises($filtre);
+
+// Si il y a au moins une entreprise
+if (sizeof($tabEntreprises) > 0) {
+    // Affichage des entreprises correspondants aux critères de recherches
+    Entreprise_IHM::afficherListeEntrepriseAEditer($tabEntreprises);
+}else {
+    echo "Aucune entreprise ne correspond aux critères de recherche.";
+}
+
+?>
