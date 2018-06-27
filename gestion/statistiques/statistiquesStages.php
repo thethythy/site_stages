b767dfa104de8414c0e7ff32264255b478b6f672
Thierry Lemeunier <thierry.lemeunier@univ-lemans.fr>
2018-06-27 10:41:09 +0200

    Renommer statistiques.js en statistiquesStages.js

--- gestion/statistiques/statistiquesStages.php
+++ gestion/statistiques/statistiquesStages.php
@@ -24,7 +24,7 @@ Promotion_IHM::afficherFormulaireSelectionInterval();
 
 // Chargement des traitements (affichage et contrôle)
 echo "<div id='data'></div>\n";
-echo "<script type='text/javascript' src='statistiques.js'></script>\n";
+echo "<script type='text/javascript' src='statistiquesStages.js'></script>\n";
 
 deconnexion();
 
