49e99390ac498e16eb3ac31ae789ba0343f24814
Thierry Lemeunier <thethythy@gmail.com>
2023-06-19 15:04:30 +0200

    Mise à jour de la page de téléchargement

--- telechargements/index.php
+++ telechargements/index.php
@@ -49,10 +49,8 @@ IHM_Generale::header("", "Téléchargements", "../", $tabLiens);
 <h4>Documents officiels</h4>
 
 <ul>
-<li>Guide des stages 2012 du Ministère de L'Education Nationale et de l'Enseignement Supérieur (format PDF) <A href="/documents/telechargements/GuideStages2012.pdf"><IMG border=0 title="Guide des stages" align=middle src="../images/download.png"></A></li>
-<li>Charte des stages 2006-2007 du Ministère de L'Education Nationale (format PDF) <A href="/documents/telechargements/Charte-des-stages.pdf"><IMG border=0 title="Charte des stages" align=middle src="../images/download.png"></A></li>
-<li>Décret n°2009-885 du 21 juillet 2009 sur l'accueil des étudiants dans les établissements publics de l'Etat (format PDF) <A href="/documents/telechargements/Decret-2009-885.pdf"><IMG border=0 title="Décret n°2009-885" align=middle src="../images/download.png"></A></li>
-<li>Décret n°2010-956 du 25 août 2010 sur les stages hors cursus (format PDF) <A href="/documents/telechargements/Decret-2010-956.pdf"><IMG border=0 title="Décret n°2010-956" align=middle src="../images/download.png"></A></li>
+<li>Guide des stages 2022 du Ministère de L'Education Nationale et de l'Enseignement Supérieur (format PDF) <A href="/documents/telechargements/GuideStages2022.pdf"><IMG border=0 title="Guide des stages" align=middle src="../images/download.png"></A></li>
+<li>Accueil des stagiaires dans le fonction public 2021 (format PDF) <A href="/documents/telechargements/GuideAccueilStagiairesFonctionPublique2021.pdf"><IMG border=0 title="Accueil des stages dans la fonction public" align=middle src="../images/download.png"></A></li>
 <li>Loi n°2014-788 du 10 juillet 2014 tendant au développement, à l'encadrement des stages et à l'amélioration du statut des stagiaires (format PDF) <a href="/documents/telechargements/Loi-2014-788.pdf"><IMG border=0 title="Loi n°2014-788" align=middle src="../images/download.png"></a></li>
 </ul>
 
